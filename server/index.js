5dd7d5900d9a1bbbab0e70209e06ed5e0284bb73
alpharmi <gianni.s177@outlook.com>
2023-03-10 15:44:03 +1100

    update

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require("express")
+//const cors = require("cors")
+//const rateLimit = require('express-rate-limit')
 const app = express()
 
+//const limiter = rateLimit({windowMs: 15 * 60 * 1000, max: 25})
+
+//app.use(cors())
 app.listen(3000)
