b2a1c97a48dbb62290db97de450bd4f25e02ff04
alpharmi <gianni.s177@outlook.com>
2023-03-10 15:35:35 +1100

    initial

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="min-h-screen">
+    <RouterView/>
+  </main>
+</template>
+
+<script setup>
+  import * as vueRouter from "vue-router"
+
+  const router = vueRouter.useRouter()
+</script>
